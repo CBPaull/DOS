38270efb1cf21d1d11c50ed12497a2b29d0d9b55
Adam Shechter <adam.s.develop@gmail.com>
2016-06-28 19:41:56 -0700

    jobs and users controllers.

--- app/controllers/Users.py
+++ app/controllers/Users.py
@@ -3,6 +3,9 @@ from system.core.controller import *
 class Users(Controller):
     def __init__(self, action):
         super(Users, self).__init__(action)
+        self.load_model('Job')
+        self.load_model('User')
+        self.db = self._app.db
 
     def index(self):
         return self.load_view('/users/login_registration.html')
